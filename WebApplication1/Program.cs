c6b790ff0934cbe8ca1085d7dd4918a334488a36
Artem Kurta <artemdog386@gmail.com>
2025-11-18 20:09:15 +0200

    feat: add css style for site

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,40 +15,6 @@ var app = builder.Build();
 
 app.UseStaticFiles(); // css
 
-
-// для тестовых данных
-//using (var scope = app.Services.CreateScope())
-//{
-//    var context = scope.ServiceProvider.GetRequiredService<GameDevContext>();
-
-//    // cоздаем бд если не существует
-//    context.Database.EnsureCreated();
-
-//    //  таблица пустая
-//    if (!context.Projects.Any())
-//    {
-//        context.Projects.AddRange(
-//            new Project
-//            {
-//                Name = "test",
-//                Budget = 100000,
-//                Deadline = DateTime.Now.AddMonths(6),
-//                Status = "Development"
-//            },
-//            new Project
-//            {
-//                Name = "test_2",
-//                Budget = 90000,
-//                Deadline = DateTime.Now.AddMonths(3),
-//                Status = "Development"
-//            }
-//        );
-//        context.SaveChanges();
-//        Console.WriteLine("Добавлены тестовые проекты");
-//    }
-//}
-
-
 // Налаштування HTTP pipeline
 if (!app.Environment.IsDevelopment())
 {
@@ -63,5 +29,10 @@ app.UseAuthorization();
 
 // МАРШРУТИЗАЦІЯ
 app.MapCustomRoutes();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
